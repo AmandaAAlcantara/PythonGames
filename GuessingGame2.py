b900efc074286a46b216526da4184a9656cadf72
Amanda Alcantara <alcantaraaa@cardiff.ac.uk>
2024-03-06 15:17:16 +0000

    Second Guessing Game

---
+++ GuessingGame2.py
@@ -0,0 +1,26 @@
+from random import randint
+print('Jogo de Adivinhação')
+
+inicial = int(input('Digite o menor número possível (inteiro e positivo) para adivinhação: '))
+
+if inicial <0:
+    print('Erro, digite somente números positivos')
+else: 
+
+    final = int(input('Digite o maior número possível para adivinhação: '))
+
+    sorteio = randint (inicial, final+1)
+    adivinhe = int(input('Adivinhe qual número foi gerado: '))
+
+    print('Número gerado =',sorteio)
+    if adivinhe == sorteio:
+        print('Seu palpite estava correto')
+    elif adivinhe > sorteio:
+        print('Seu palpite foi muito alto')
+    else:
+        print('Seu palpite foi muito baixo')
+
+
+
+
+
